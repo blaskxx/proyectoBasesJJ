03f65d677f7de2e19bd7a3e4a7f07f531523fe9c
José Madrigal <pablomadrigaless@gmail.com>
2014-03-27 11:07:28 -0600

    plantilla proyecto

---
+++ ProyectoBaseDeDatos/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   newmain.cpp
+ * Author: jose
+ *
+ * Created on 27 de marzo de 2014, 11:04 AM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
